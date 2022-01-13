88cb6955bb8371e553987288225855c7a1533491
Khalid_Syahmi <mksyahmiof@gmail.com>
2022-01-13 09:39:41 +0800

    .gitignore-finally

---
+++ app.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "index.html");
+  res.sendFile(htmlFilePath);
+
+  /* res.send("<h1>Hello world</h1>"); */
+});
+
+app.get("/restaurants", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/recommend", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/confirm", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
+  res.sendFile(htmlFilePath);
+});
+app.get("/about", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "about.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.listen(3000);
